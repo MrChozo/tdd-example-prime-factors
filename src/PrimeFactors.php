af46146cca3abdf182353c6fa99886574b07d48b
Brian Weston <brianw@webriculture.com>
2022-12-14 09:27:28 -0800

    Refactor tests using @dataProvider

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -10,11 +10,6 @@ class PrimeFactors
         $factors = [];
         $divisor = 2;
 
-        // 1. Is the number divisible by 2?
-        // 2. If true, then divide by 2. If false, increase prime number candidate
-        //    and try again.
-        // 3. Repeat.
-
         while ($int > 1) {
             while ($int % $divisor === 0) {
                 $factors[] = $divisor;
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -6,35 +6,28 @@ use PHPUnit\Framework\TestCase;
 
 class PrimeFactorsTest extends TestCase
 {
-    /** @test */
-    public function it_generates_prime_factors_for_1()
+    /**
+     * @test
+     * @dataProvider factors
+     */
+    public function it_generates_prime_factors_for_n($int, $expected)
     {
         $factors = new PrimeFactors;
 
-        $this->assertEquals([], $factors->generate(1));
+        $this->assertEquals($expected, $factors->generate($int));
     }
 
-    /** @test */
-    public function it_generates_prime_factors_for_2()
+    public function factors()
     {
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([2], $factors->generate(2));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_3()
-    {
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([3], $factors->generate(3));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_4()
-    {
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([2, 2], $factors->generate(4));
+        return [
+            [ 1,   []           ],
+            [ 2,   [2]          ],
+            [ 3,   [3]          ],
+            [ 4,   [2, 2]       ],
+            [ 5,   [5]          ],
+            [ 6,   [2, 3]       ],
+            [ 8,   [2, 2, 2]    ],
+            [ 100, [2, 2, 5, 5] ],
+        ];
     }
 }
